0d7aaf7df274ee34004b11b09eca40c289cff67e
anna-seryodkina <a.serodkina@gmail.com>
2023-04-14 18:53:18 +0300

    done i guess

--- db/database.js
+++
@@ -1,5 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const app = require('express')();
-const db = new sqlite3.Database('./db/students.db');
-
-
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var jwt = require('jsonwebtoken');
 const app = require('express')();
 const PORT = 3001;
+const bodyParser = require('body-parser');
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/students.db');
+let sql;
+
+
+app.use(bodyParser.json())
 
 
 app.get('/', function(req, res){
@@ -9,28 +17,46 @@ app.get('/', function(req, res){
 
 
 app.get('/students', (req, res) => {
-    res.status(200).send({
-        all_students: [] // connect db
-    })
+    sql = "SELECT * FROM students";
+    try {
+        db.all(sql, [], (err, rows) => {
+            if(err) return res.json({status: 300,  success: false, error: err});
+
+            if(rows.length < 1) return res.json({status: 300,  success: false, error: "no match"});
+
+            return res.json({ status: 200, data: rows, success: true });
+        })
+    } catch (error) {
+        return res.json({
+            status: 400,
+            success: false
+        });
+    }
 });
 
 
 app.get('/:nickname', (req, res) => {
-    var token = jwt.sign({'nickname': req.params.nickname}, 'supersecret',{expiresIn: 120});
-    res.json({'token': token});
+    var token = jwt.sign({'nickname': req.params.nickname}, 'supersecret');
+    res.json({'nickname': req.params.nickname, 'token': token});
 });
 
 
-app.post('/:nickname/:firstname/:lastname', (req, res) => {
-    var token = jwt.sign({'nickname': req.params.nickname}, 'supersecret',{expiresIn: 120});
+app.post('/:nickname', (req, res) => {
+    var token = jwt.sign({'nickname': req.params.nickname}, 'supersecret');
     jwt.verify(token, 'supersecret', function(err, decoded){
-    if(!err){
-        // add to DB
-        var added = {'added' : 'yes', 'nickname': req.params.nickname, 'firstname': req.params.firstname};
-        res.json(added);
-    } else {
-        res.send(err);
-    }
+        if(!err){
+            // add to DB
+            const {nickname, firstname, lastname} = req.body;
+            sql = "INSERT INTO students(nickname, firstname, lastname) VALUES (?,?,?)"
+            db.run(sql, [nickname, firstname, lastname], (err)=>{
+                if(err) return res.json({status: 300,  success: false, error: err});
+
+                console.log("added successfully.");
+                res.json({status: 200,  success: true})
+            })
+        } else {
+            res.send(err);
+        }
     })
 });
 
